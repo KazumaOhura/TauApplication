caeb3943c18f454be3695047c075bd5773cc93a6
Kazuma Ohura <k.ohura0303@gmail.com>
2022-01-25 03:41:13 +0900

    🏷️ Move GameBase class to App files.

--- TauApplication/App/App.cpp
+++ TauApplication/App/App.cpp
@@ -1,7 +1,38 @@
-#include "../Header/App.h"
+﻿#include "../Header/App.h"
 
-#include <windows.h>
 namespace Tau {
 HINSTANCE App::m_hInstance = nullptr;
 Type::tstring App::m_name = _T("");
+
+DWORD GameBase::m_frame = 0;
+
+void GameBase::Run() {
+  //ゲームのメインループ
+  DWORD Prev = timeGetTime();
+  DWORD Curr = 0;
+
+  MSG msg = {0};
+
+  timeBeginPeriod(1);
+
+  ZeroMemory(&msg, sizeof(msg));
+  while (msg.message != WM_QUIT) {
+    if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE)) {
+      TranslateMessage(&msg);
+      DispatchMessage(&msg);
+    } else {
+      Curr = timeGetTime();
+      // todo:ゲームの処理
+      if (Curr - Prev >= 1000 / 60) {
+        this->UpdateLogic();
+        this->UpdateRender();
+        ++GameBase::m_frame;
+      }
+      Prev = Curr;
+    }
+    Sleep(1);
+  }
+  timeEndPeriod(1);
+}
+
 }  // namespace Tau
